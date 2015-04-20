847547861bd110df23b5de164739f1f61b600bf4
Sasan Rose <sasan.rose@gmail.com>
2015-04-20 18:11:01 +0430

    Show script info in a modal

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -24,6 +24,23 @@
     timezoneJS.timezone.defaultZoneFile = [];
     timezoneJS.timezone.init({async: false});
 
+    app.directive('scriptInfoModal', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                name: '@name'
+            },
+            templateUrl: 'static/tmpl/script-info-modal.html',
+            controller: ['$http', '$scope', function($http, $scope) {
+                $http.get('/scripts/'+$scope.name).success(function(data) {
+                    $scope.details = data;
+                });
+            }],
+            controllerAs: 'script',
+        };
+    });
+
+
     app.directive('scriptInfo', function() {
         return {
             restrict: 'E',
@@ -31,12 +48,6 @@
                 name: '@name'
             },
             templateUrl: 'static/tmpl/script-info.html',
-            controller: ['$http', '$scope', function($http, $scope) {
-                $http.get('/scripts/'+$scope.name).success(function(data) {
-                    $scope.details = data;
-                });
-            }],
-            controllerAs: 'script',
         };
     });
 
